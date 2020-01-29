aff2eab9b019a0e1d750b61459c2071edcad2d74
marwinq <marwin.quito@gmail.com>
2020-01-29 14:05:32 -0600

    Create Service-Touch.ps1

---
+++ Service-Touch.ps1
@@ -0,0 +1,61 @@
+
+##Mass start, stop, or restart services across various servers.
+
+#serverFile with server names separate line by line
+$serverFile ="C:\Servers.txt"
+$serviceFile="C:\Services.txt"
+
+$input = Read-Host -prompt '1) Start Services 2) Stop Services 3) Restart Service'
+
+if (($input -eq 1 ) -or ($input -eq 2 ) -or ($input -eq 3 )){
+
+    Service-Action($input)
+
+}
+
+else{
+
+"Not a valid option."
+
+}
+
+function Service-Action{
+
+Param($option)
+
+foreach($serviceName in Get-Content $serviceFile){
+
+    foreach($server in Get-Content $serverFile){
+    
+    $service = Get-Service -Name $serviceName -ComputerName $server -ErrorAction SilentlyContinue
+    
+    if ($option -eq 1){
+    
+        "Starting "+$serviceName+" on "+$server
+        $service | Start-Service
+    }
+    if ($option -eq 2){
+        "Stopping "+$serviceName+" on "+$server
+        $service | Format-List Property Name,DependentServices
+        $service | Stop-Service -Force
+    }
+    if ($option -eq 3){
+        "Restarting "+$serviceName+" on "+$server
+        $service | Restart-Service
+    }
+
+    if (-Not $service) {$serviceName + " does not exist on "+$server+". Skipping.."}
+    
+    }
+}
+
+foreach($serviceName in Get-Content $serviceFile){
+
+    foreach($server in Get-Content $serverFile){
+    
+    $service = Get-Service -Name $serviceName -ComputerName $server -ErrorAction SilentlyContinue 
+    $service | select Displayname,Status, @{l="Server";e={$server}} 
+    
+    }
+}
+}
